798096f7cefe05b1eb2a8605fa18a5c8d5bd19b0
Igor Lira <lira@hey.com>
2024-07-25 17:16:12 -0700

    add extension

---
+++ vite.config.extension.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./extension/manifest.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "dist-extension",
+  },
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+    // Build Chrome Extension
+    crx({ manifest }),
+  ],
+});
