8c3d129503e18bf5c882f28780de290ac9bc45d3
Igor Lira <lira@hey.com>
2024-07-27 00:35:44 -0700

    remove unused stuff

--- vm-rust/src/player/datum_ref.rs
+++ vm-rust/src/player/datum_ref.rs
@@ -1,39 +1,19 @@
 use std::{cell::{Cell, UnsafeCell}, fmt::Display, rc::Rc};
 
-use super::{allocator::{DatumAllocatorEvent, DatumAllocatorTrait}, ALLOCATOR_TX, PLAYER_OPT};
+use super::{allocator::DatumAllocatorTrait, PLAYER_OPT};
 
 pub type DatumId = usize;
 
 pub enum DatumRef {
   Void,
-  Ref(DatumId, *mut u32),//Rc<UnsafeCell<u32>>),
+  Ref(DatumId, *mut u32),
 }
-// pub static VOID_DATUM_REF: DatumRef = DatumRef::Void;
-// pub static mut DATUM_REF_COUNT: Option<IntMap<DatumId, u32>> = None;
-
-// fn on_datum_ref_added(id: DatumId) {
-//   unsafe {
-//     let ref_count_map = DATUM_REF_COUNT.as_mut().unwrap();
-//     let count = ref_count_map.get_mut(&id).unwrap();
-//     *count += 1;
-//   }
-// }
-
-
 
 impl DatumRef {
   pub fn from_id(id: DatumId, ref_count: *mut u32) -> DatumRef {
     if id != 0 {
-      // unsafe {
-      //   ALLOCATOR_TX.as_ref().unwrap().try_send(DatumAllocatorEvent::RefAdded(id)).unwrap();
-      // }
-      // unsafe {
-        // let player = PLAYER_OPT.as_mut().unwrap();
-        // player.allocator.on_datum_ref_added(id);
-        // let ref_count = ref_count.get();
       let mut_ref = unsafe { &mut *ref_count };
       *mut_ref += 1;
-      // }
       DatumRef::Ref(id, ref_count)
     } else {
       DatumRef::Void
@@ -84,15 +64,11 @@ impl Drop for DatumRef {
   fn drop(&mut self) {
     if let DatumRef::Ref(id, ref_count) = self {
       unsafe {
-        // let player = PLAYER_OPT.as_mut().unwrap();
-        // player.allocator.on_datum_ref_dropped(*id);
-        // let ref_count = ref_count.get();
         let ref_count = &mut **ref_count;
         *ref_count -= 1;
         if *ref_count <= 0 {
           let player = PLAYER_OPT.as_mut().unwrap();
           player.allocator.on_datum_ref_dropped(*id);
-          // ALLOCATOR_TX.as_ref().unwrap().try_send(DatumAllocatorEvent::RefDropped(*id)).unwrap();
         }
       }
     }
