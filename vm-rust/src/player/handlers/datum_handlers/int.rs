23fd496b8a7cefb06a01153a94ec1d0370899f7e
Igor Lira <lira@hey.com>
2024-08-06 18:28:13 -0700

    int datum props

--- vm-rust/src/player/handlers/datum_handlers/int.rs
+++ vm-rust/src/player/handlers/datum_handlers/int.rs
@@ -3,13 +3,18 @@ use crate::{director::lingo::datum::Datum, player::{DatumRef, DirPlayer, ScriptE
 pub struct IntDatumHandlers {}
 
 impl IntDatumHandlers {
-  pub fn get_prop(player: &mut DirPlayer, _: &DatumRef, prop: &String) -> Result<DatumRef, ScriptError> {
+  pub fn get_prop(player: &mut DirPlayer, datum_ref: &DatumRef, prop: &String) -> Result<DatumRef, ScriptError> {
     match prop.as_str() {
       "ilk" => {
         Ok(player.alloc_datum(Datum::Symbol("integer".to_string())))
       },
       "integer" => {
-        Ok(player.alloc_datum(Datum::Int(1)))
+        let value = player.get_datum(datum_ref).int_value()?;
+        Ok(player.alloc_datum(Datum::Int(value)))
+      },
+      "string" => {
+        let value = player.get_datum(datum_ref).int_value()?;
+        Ok(player.alloc_datum(Datum::String(value.to_string())))
       },
       _ => {
         Err(ScriptError::new(format!("Cannot get int property {}", prop)))
