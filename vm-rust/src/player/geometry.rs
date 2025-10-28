c9272ed0642135eb7f406ac318e887ecc967abe4
Igor Lira <lira@hey.com>
2025-10-27 17:14:12 -0700

    update quad parsing

--- vm-rust/src/player/geometry.rs
+++ vm-rust/src/player/geometry.rs
@@ -2,7 +2,7 @@ use log::warn;
 
 use crate::console_warn;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct IntRect {
   pub left: i32,
   pub top: i32,
@@ -31,17 +31,29 @@ impl IntRect {
     bottom_right: (i32, i32),
     bottom_left: (i32, i32),
   ) -> IntRect {
-    if top_left.1 != top_right.1 || top_right.0 != bottom_right.0 || bottom_right.1 != bottom_left.1 || bottom_left.0 != top_left.0 {
-      warn!("INVALID IntRect::from_quad(({}, {}), ({}, {}), ({}, {}), ({}, {}))", top_left.0, top_left.1, top_right.0, top_right.1, bottom_right.0, bottom_right.1, bottom_left.0, bottom_left.1);
+    // For axis-aligned rectangles (including flipped ones):
+    // - Top edge: top_left.y should equal top_right.y
+    // - Bottom edge: bottom_left.y should equal bottom_right.y
+    // - Left edge: top_left.x should equal bottom_left.x
+    // - Right edge: top_right.x should equal bottom_right.x
+    
+    // Validate it's an axis-aligned quad
+    if top_left.1 != top_right.1 || bottom_left.1 != bottom_right.1 || top_left.0 != bottom_left.0 || top_right.0 != bottom_right.0 {
+      warn!("INVALID IntRect::from_quad - not axis-aligned: TL({}, {}), TR({}, {}), BR({}, {}), BL({}, {})", 
+        top_left.0, top_left.1, top_right.0, top_right.1, 
+        bottom_right.0, bottom_right.1, bottom_left.0, bottom_left.1);
       return IntRect::from(0, 0, 0, 0);
     }
 
-    return IntRect {
-      left: top_left.0,
-      top: top_left.1,
-      right: bottom_right.0,
-      bottom: bottom_right.1,
-    };
+    // For flipped rectangles, the "left" might actually be on the right side
+    // We need to preserve this to indicate flipping
+    // Just use the actual corner positions regardless of whether they're "correct"
+    IntRect {
+      left: top_left.0,      // May be > right if horizontally flipped
+      top: top_left.1,       // May be > bottom if vertically flipped
+      right: top_right.0,    // May be < left if horizontally flipped
+      bottom: bottom_right.1, // May be < top if vertically flipped
+    }
   }
 
   pub const fn width(&self) -> i32 {
