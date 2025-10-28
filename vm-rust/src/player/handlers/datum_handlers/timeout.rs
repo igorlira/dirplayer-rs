3330d36dd4396b1ca75703b896910ab34fed21aa
Igor Lira <lira@hey.com>
2025-10-27 17:14:12 -0700

    timeout tied to script

--- vm-rust/src/player/handlers/datum_handlers/timeout.rs
+++ vm-rust/src/player/handlers/datum_handlers/timeout.rs
@@ -1,4 +1,4 @@
-use crate::{console_warn, director::lingo::datum::Datum, player::{reserve_player_mut, timeout::Timeout, DatumRef, DirPlayer, ScriptError}};
+use crate::{console_warn, director::lingo::datum::Datum, player::{reserve_player_mut, reserve_player_ref, timeout::Timeout, DatumRef, DirPlayer, ScriptError}};
 
 pub struct TimeoutDatumHandlers {}
 
@@ -6,26 +6,87 @@ impl TimeoutDatumHandlers {
   #[allow(dead_code, unused_variables)]
   pub fn call(datum: &DatumRef, handler_name: &String, args: &Vec<DatumRef>) -> Result<DatumRef, ScriptError> {
     match handler_name.as_str() {
-      "new" => Self::new(datum, args),
       "forget" => Self::forget(datum, args),
       _ => Err(ScriptError::new(format!("No handler {handler_name} for timeout")))
     }
   }
 
-  pub fn new(datum: &DatumRef, args: &Vec<DatumRef>) -> Result<DatumRef, ScriptError> {
-    reserve_player_mut(|player| {
-      let timeout_period = player.get_datum(&args[0]).int_value()?;
-      let timeout_handler = player.get_datum(&args[1]).string_value()?;
-      let target_ref = args[2].clone();
-      let timeout_datum = player.get_datum(&datum);
-      let timeout_name = match timeout_datum {
-        Datum::TimeoutRef(timeout_name) => timeout_name,
-        _ => return Err(ScriptError::new("Cannot create timeout from non-timeout".to_string())),
-      };
+  pub fn has_async_handler(name: &String) -> bool {
+    match name.as_str() {
+      "new" => true,
+      _ => false,
+    }
+  }
 
+  pub async fn call_async(datum: &DatumRef, handler_name: &String, args: &Vec<DatumRef>) -> Result<DatumRef, ScriptError> {
+    match handler_name.as_str() {
+      "new" => Self::new(datum, args).await,
+      _ => Err(ScriptError::new(format!("No async handler {handler_name} for timeout")))
+    }
+  }
+
+  pub async fn new(datum: &DatumRef, args: &Vec<DatumRef>) -> Result<DatumRef, ScriptError> {
+    let timeout_name = reserve_player_ref(|player| {
+      let timeout_datum = player.get_datum(&datum);
+      match timeout_datum {
+        Datum::TimeoutRef(timeout_name) => Ok(timeout_name.clone()),
+        _ => Err(ScriptError::new("Cannot create timeout from non-timeout".to_string())),
+      }
+    })?;
+
+    // Check if this timeout name corresponds to a script in the cast
+    let script_ref = reserve_player_ref(|player| {
+      player.movie.cast_manager.find_member_ref_by_name(&timeout_name)
+    });
+
+    if let Some(script_ref) = script_ref {
+      // Verify it's actually a script member
+      let is_script = reserve_player_ref(|player| {
+        player.movie.cast_manager.get_script_by_ref(&script_ref).is_some()
+      });
+
+      if is_script {
+        // This is a script-based timeout (like _TIMER_)
+        // Pass ALL arguments to the script's new() handler
+        use crate::player::handlers::datum_handlers::script::ScriptDatumHandlers;
+        let script_datum = reserve_player_mut(|player| {
+          Ok(player.alloc_datum(Datum::ScriptRef(script_ref)))
+        })?;
+
+        // IMPORTANT: Pass the original args directly to the script's new() handler
+        // The script's new() expects: new(me, _iTimeOut, _hTargetHandler, _oTargetObject, ...)
+        let script_instance = ScriptDatumHandlers::new(&script_datum, args).await?;
+        
+        // The script's new() handler will:
+        // 1. Set all properties (iStartTime, iTimeOut, etc.)
+        // 2. Call (the actorList).add(me)
+        // 3. Return me
+        
+        // So we should return the script instance that was returned from new()
+        return Ok(script_instance);
+      }
+    }
+
+    // Not a script-based timeout - create a traditional JavaScript timeout
+    // This is for backward compatibility with non-script timeouts
+    let timeout_period = reserve_player_ref(|player| {
+      player.get_datum(&args[0]).int_value()
+    })?;
+
+    let timeout_handler = reserve_player_ref(|player| {
+      match player.get_datum(&args[1]) {
+        Datum::String(s) => Ok(s.clone()),
+        Datum::Symbol(s) => Ok(s.clone()),
+        _ => Err(ScriptError::new("Timeout handler must be a string or symbol".to_string())),
+      }
+    })?;
+
+    let target_ref = args[2].clone();
+    
+    reserve_player_mut(|player| {
       let mut timeout = Timeout {
         handler: timeout_handler,
-        name: timeout_name.to_owned(),
+        name: timeout_name.clone(),
         period: timeout_period as u32,
         target_ref,
         is_scheduled: false,
