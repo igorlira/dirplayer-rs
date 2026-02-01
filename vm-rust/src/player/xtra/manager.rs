a3f68d9ad6c450fa8f6171c084971bd460e1698d
Igor Lira <lira@hey.com>
2026-02-01 13:31:39 -0800

    xmlparser xtra

--- vm-rust/src/player/xtra/manager.rs
+++ vm-rust/src/player/xtra/manager.rs
@@ -4,9 +4,15 @@ use crate::{
 };
 
 use super::multiuser::{borrow_multiuser_manager_mut, MultiuserXtraManager};
+use super::xmlparser::{borrow_xmlparser_manager_mut, XmlParserXtraManager};
 
 pub fn is_xtra_registered(name: &String) -> bool {
-    return name == "Multiuser";
+    let name_lower = name.to_lowercase();
+    return name == "Multiuser" || name_lower == "xmlparser";
+}
+
+pub fn get_registered_xtra_names() -> Vec<&'static str> {
+    vec!["Multiusr", "XmlParser"]
 }
 
 pub fn call_xtra_instance_handler(
@@ -15,9 +21,13 @@ pub fn call_xtra_instance_handler(
     handler_name: &String,
     args: &Vec<DatumRef>,
 ) -> Result<DatumRef, ScriptError> {
-    match xtra_name.as_str() {
-        "Multiuser" => {
+    let xtra_name_lower = xtra_name.to_lowercase();
+    match xtra_name_lower.as_str() {
+        "multiuser" => {
             return MultiuserXtraManager::call_instance_handler(handler_name, instance_id, args)
+        }
+        "xmlparser" => {
+            return XmlParserXtraManager::call_instance_handler(handler_name, instance_id, args)
         }
         _ => Err(ScriptError::new(format!(
             "No handler {} found for xtra {} instance #{}",
@@ -32,9 +42,18 @@ pub async fn call_xtra_instance_async_handler(
     handler_name: &String,
     args: &Vec<DatumRef>,
 ) -> Result<DatumRef, ScriptError> {
-    match xtra_name.as_str() {
-        "Multiuser" => {
+    let xtra_name_lower = xtra_name.to_lowercase();
+    match xtra_name_lower.as_str() {
+        "multiuser" => {
             return MultiuserXtraManager::call_instance_async_handler(
+                handler_name,
+                instance_id,
+                args,
+            )
+            .await
+        }
+        "xmlparser" => {
+            return XmlParserXtraManager::call_instance_async_handler(
                 handler_name,
                 instance_id,
                 args,
@@ -53,8 +72,10 @@ pub fn has_xtra_instance_async_handler(
     handler_name: &String,
     _instance_id: XtraInstanceId,
 ) -> bool {
-    match xtra_name.as_str() {
-        "Multiuser" => MultiuserXtraManager::has_instance_async_handler(handler_name),
+    let xtra_name_lower = xtra_name.to_lowercase();
+    match xtra_name_lower.as_str() {
+        "multiuser" => MultiuserXtraManager::has_instance_async_handler(handler_name),
+        "xmlparser" => XmlParserXtraManager::has_instance_async_handler(handler_name),
         _ => false,
     }
 }
@@ -63,8 +84,10 @@ pub fn create_xtra_instance(
     xtra_name: &String,
     args: &Vec<DatumRef>,
 ) -> Result<XtraInstanceId, ScriptError> {
-    match xtra_name.as_str() {
-        "Multiuser" => Ok(borrow_multiuser_manager_mut(|x| x.create_instance(args))),
+    let xtra_name_lower = xtra_name.to_lowercase();
+    match xtra_name_lower.as_str() {
+        "multiuser" => Ok(borrow_multiuser_manager_mut(|x| x.create_instance(args))),
+        "xmlparser" => Ok(borrow_xmlparser_manager_mut(|x| x.create_instance(args))),
         _ => Err(ScriptError::new(format!("Xtra {} not found", xtra_name))),
     }
 }
