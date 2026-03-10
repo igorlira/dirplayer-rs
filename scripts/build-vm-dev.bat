22393d29e695198beafa1a984852c1dd555e6de0
Tom Niget <zippedfire@free.fr>
2026-03-10 16:26:48 +0100

    Add dev build scripts for VM (with DWARF info)

---
+++ scripts/build-vm-dev.bat
@@ -0,0 +1,14 @@
+@echo off
+title build-vm
+
+:: Navigate to the folder of this script.
+cd %~dp0
+
+:: Navigate back to the "vm-rust" folder.
+cd .\..\vm-rust\
+
+:: Run the build command.
+powershell wasm-pack build --target web --dev
+
+pause
+exit
