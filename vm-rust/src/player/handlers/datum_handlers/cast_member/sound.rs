65b1535143b6aea166f28c2c4e42ebca2d8165f8
Matthew Dean <matthewdean@users.noreply.github.com>
2026-04-05 14:28:32 -0600

    fix: support get/set of loop prop on sound cast members (#101)
    Lingo scripts can read and write `member("x").loop` on sound members.
    Previously this fell through to the default handler and errored.

--- vm-rust/src/player/handlers/datum_handlers/cast_member/sound.rs
+++ vm-rust/src/player/handlers/datum_handlers/cast_member/sound.rs
@@ -1,6 +1,6 @@
 use crate::{
     director::lingo::datum::Datum,
-    player::{cast_lib::CastMemberRef, DirPlayer, ScriptError},
+    player::{cast_lib::CastMemberRef, reserve_player_mut, DirPlayer, ScriptError},
 };
 
 pub struct SoundMemberHandlers {}
@@ -24,8 +24,36 @@ impl SoundMemberHandlers {
             "sampleSize" => Ok(Datum::Int(sound.info.sample_size as i32)),
             "channelCount" => Ok(Datum::Int(sound.info.channels as i32)),
             "sampleCount" => Ok(Datum::Int(sound.info.sample_count as i32)),
+            "loop" => Ok(Datum::Int(if sound.info.loop_enabled { 1 } else { 0 })),
             _ => Err(ScriptError::new(format!(
                 "Cannot get castMember property {} for sound",
+                prop
+            ))),
+        }
+    }
+
+    pub fn set_prop(
+        member_ref: &CastMemberRef,
+        prop: &str,
+        value: Datum,
+    ) -> Result<(), ScriptError> {
+        match prop {
+            "loop" => {
+                let loop_enabled = value.bool_value()?;
+                reserve_player_mut(|player| {
+                    let member = player
+                        .movie
+                        .cast_manager
+                        .find_mut_member_by_ref(member_ref)
+                        .ok_or_else(|| ScriptError::new("Cast member not found".to_string()))?;
+                    let sound = member.member_type.as_sound_mut()
+                        .ok_or_else(|| ScriptError::new("Cast member is not a sound".to_string()))?;
+                    sound.info.loop_enabled = loop_enabled;
+                    Ok(())
+                })
+            }
+            _ => Err(ScriptError::new(format!(
+                "Cannot set castMember prop {} for sound",
                 prop
             ))),
         }
